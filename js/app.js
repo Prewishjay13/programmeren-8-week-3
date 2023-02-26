8802b47458f4479ce163b87cb99188290e2b3cbb
Prewishjay13 <45820145+Prewishjay13@users.noreply.github.com>
2023-02-26 13:02:53 +0100

    .

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 const video = document.getElementById("webcam");
 const label = document.getElementById("label");
-
+const videoStatus = document.getElementById("videoStatus");
 const labelOneBtn = document.querySelector("#labelOne");
 const labelTwoBtn = document.querySelector("#labelTwo");
 const labelThreeBtn = document.querySelector("#labelThree");
+const amountOne = document.getElementById("amountOne");
+const amountTwo = document.getElementById("amountTwo");
+const amountThree = document.getElementById("amountThree");
 const trainbtn = document.querySelector("#train");
+const loss = document.getElementById("loss");
+const result = document.getElementById("result");
+const confidence = document.getElementById("confidence");
+const predict = document.getElementById("predict");
 
+let totalLoss = 0;
 labelOneBtn.addEventListener("click", () => console.log("button 1"));
 labelTwoBtn.addEventListener("click", () => console.log("button 2"));
 labelThreeBtn.addEventListener("click", () => console.log("button 3"));
@@ -24,3 +32,60 @@ if (navigator.mediaDevices.getUserMedia) {
 }
 
 label.innerText = "Ready when you are!";
+
+function modelLoaded() {
+    loading.innerText = "Model loaded!";
+  }
+  
+  const featureExtractor = ml5.featureExtractor("MobileNet", modelLoaded);
+
+  const classifier = featureExtractor.classification(video, videoReady);
+
+  // A function to be called when the video is finished loading
+function videoReady() {
+    videoStatus.innerText = "Video ready!";
+  }
+
+  labelOneBtn.onclick = function() {
+    classifier.addImage("one");
+    amountOne.innerText = Number(amountOne.innerText) + 1;
+  };
+
+  labelTwoBtn.onclick = function() {
+    classifier.addImage("two");
+    amountTwo.innerText = Number(amountTwo.innerText) + 1;
+  };
+
+  labelThreeBtn.onclick = function() {
+    classifier.addImage("three");
+    amountThree.innerText = Number(amountThree.innerText) + 1;
+  };
+
+  train.onclick = function() {
+    classifier.train(function(lossValue) {
+      if (lossValue) {
+        totalLoss = lossValue;
+        loss.innerHTML = `Loss: ${totalLoss}`;
+      } else {
+        loss.innerHTML = `Done Training! Final Loss: ${totalLoss}`;
+      }
+    });
+  };
+  
+  // Show the results
+  function gotResults(err, results) {
+    // Display any error
+    if (err) {
+      console.error(err);
+    }
+    if (results && results[0]) {
+      result.innerText = results[0].label;
+      confidence.innerText = results[0].confidence;
+      classifier.classify(gotResults);
+    }
+  }
+  
+  // Start predicting when the predict button is clicked
+  predict.onclick = function() {
+    classifier.classify(gotResults);
+  };
